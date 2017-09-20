ed33d909997c91a3f43dc2b3b7e13e1157a3f32b
wtmarcek <wtmarcek@gmail.com>
2017-09-20 22:48:56 +0200

    shaders: radiusShader

---
+++ ShadersCookbook/Assets/Scripts/Radius.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radius : MonoBehaviour {
+
+    [SerializeField]
+    Material radiusMaterial;
+    [SerializeField]
+    float radius = 1.0f;
+    [SerializeField]
+    Color color = Color.white;
+
+	void Update ()
+    {
+        radiusMaterial.SetVector("_Center", transform.position);
+        radiusMaterial.SetFloat("_Radius", radius);
+        radiusMaterial.SetColor("_RadiusColor", color);
+	}
+}
