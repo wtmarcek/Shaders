c3953d5c99a77417f81a75deb2f263765d1e4952
Marcek <wtmarcek@gmail.com>
2017-10-25 18:57:31 +0200

    BlackHole

---
+++ ShadersCookbook/Assets/Scripts/BlackHoleEffect.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class BlackHoleEffect : MonoBehaviour {
+
+    public Shader shader;
+    public Transform blackHoleTrans;
+    public float ratio;
+    public float radius;
+
+    private Camera cam;
+    private Material _material;
+    private Material material
+    {
+        get
+        {
+            if (material == null)
+            {
+                _material = new Material(shader);
+                _material.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _material;
+        }
+    }
+
+    private void OnEnable()
+    {
+        cam = GetComponent<Camera>();
+        ratio = 1f / cam.aspect;
+    }
+
+    private void OnDisable()
+    {
+        if (_material)
+            Destroy(_material);
+    }
+
+    Vector3 blackHoleScreenPos;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (shader && material && blackHoleTrans)
+        {
+            Vector3 holeScreenPos = cam.WorldToScreenPoint(blackHoleTrans.position);
+
+            if (blackHoleScreenPos.z > 0)
+            {
+                Vector2 normalizePosition = new Vector2(blackHoleScreenPos.x / cam.pixelWidth, blackHoleScreenPos.y / cam.pixelHeight);
+
+                //Apply Shader params
+                material.SetVector("_Position", normalizePosition);
+                material.SetFloat("_Ratio", ratio);
+                material.SetFloat("_Rad", radius);
+                material.SetFloat("_Distance", Vector3.Distance(blackHoleScreenPos, transform.position));
+
+                Graphics.Blit(source, destination, material);
+            }
+
+        }
+    }
+}
